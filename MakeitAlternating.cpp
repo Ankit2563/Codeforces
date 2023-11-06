9a25e1ed10a2d9eded4cbf1a652d9da73e546eaf
ANKIT <ankitcloud2563@gmail.com>
2023-11-07 00:21:09 +0530

    #

---
+++ MakeitAlternating.cpp
@@ -0,0 +1,130 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define vi vector<int>
+#define vl vector<ll>
+#define vpi vector<pair<long long, long long>>
+#define vint vector<int>
+#define all(v) v.begin(), v.end()
+#define MOD 1000000007
+#define MOD2 998244353
+#define MX 1000000000
+#define MXL 1000000000000000000
+#define PI (ld)2 * acos(0.0)
+#define pb push_back
+#define sc second
+#define fr first
+#define endl '\n'
+#define ld long double
+#define NO cout << "NO" << endl
+#define YES cout << "YES" << endl
+void inputl(int i, int n, vector<long long> &a)
+{
+    for (int k = i; k < n; k++)
+    {
+        cin >> a[k];
+    }
+    return;
+}
+void inputi(int i, int n, vector<int> &a)
+{
+    for (int k = i; k < n; k++)
+    {
+        cin >> a[k];
+    }
+    return;
+}
+void inputvv(int k, int p, int n, int m, vector<vector<int>> &v)
+{
+    for (int i = k; i < n; i++)
+    {
+        for (int j = p; j < m; j++)
+        {
+            cin >> v[i][j];
+        }
+    }
+}
+void inputvvc(int k, int p, int n, int m, vector<vector<char>> &v)
+{
+    for (int i = k; i < n; i++)
+    {
+        for (int j = p; j < m; j++)
+        {
+            cin >> v[i][j];
+        }
+    }
+}
+
+void solve()
+{
+
+    string s;
+    cin >> s;
+  
+    int one = 0, zero = 0;
+    for (int i = 0; i < s.size();i++){
+        if(s[i]=='0'){
+            zero++;
+        }
+        else{
+            one++;
+        }
+    }
+    int n = s.size();
+
+  
+    int op = s.size();
+    int ans2 = 1;
+    int mod = 998244353;
+    for (int i = 0; i < n;)
+    {
+        ll cnt = 0;
+        if (s[i] == '0')
+        {
+           
+            while(i<n&&s[i]=='0'){
+                
+                cnt++;
+                i++;
+            }
+            
+         
+        }
+          else{
+           
+            while (i < n && s[i] == '1')
+                
+            {
+                i++;
+                cnt++;
+            }
+
+            
+           
+          }
+          ans2 = (ans2)*1LL * cnt % mod;
+
+          op--;
+    }
+
+    for (int i = 1; i <= op;i++){
+         ans2 = ans2 * 1LL * i % mod;
+    }
+
+         cout << op << " " << ans2 << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tt;
+    cin >> tt;
+    for (int i = 1; i <= tt; i++)
+    {
+        solve();
+    }
+    // solve();
+}
